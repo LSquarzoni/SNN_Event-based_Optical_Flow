86aa39ea433e485d9a98757337e12caeea76515b
LSquarzoni <lnz.squarzoni@gmail.com>
2025-07-14 10:22:03 +0200

    simplified model print code

--- model_print.py
+++ model_print.py
@@ -44,62 +44,14 @@ def test(args, config_parser):
     run = mlflow.get_run(args.runid)
     config = config_parser.merge_configs(run.data.params)
 
-    # configs
-    if config["loader"]["batch_size"] > 1:
-        config["vis"]["enabled"] = False
-        config["vis"]["store"] = False
-        config["vis"]["bars"] = False  # progress bars not yet compatible batch_size > 1
-
-    # asserts
-    if "AEE" in config["metrics"]["name"]:
-        assert (
-            config["data"]["mode"] == "gtflow_dt1" or config["data"]["mode"] == "gtflow_dt4"
-        ), "AEE computation not possible without ground truth mode"
-
-    if "AEE" in config["metrics"]["name"]:
-        assert config["data"]["window"] <= 1, "AEE computation not compatible with window > 1"
-        assert np.isclose(
-            (1.0 / config["data"]["window"]) % 1.0, 0.0
-        ), "AEE computation not compatible with windows whose inverse is not a round number"
-
-    if config["data"]["mode"] == "frames":
-        if config["data"]["window"] <= 1.0:
-            assert np.isclose(
-                (1.0 / config["data"]["window"]) % 1.0, 0.0
-            ), "Frames mode not compatible with < 1 windows whose inverse is not a round number"
-        else:
-            assert np.isclose(
-                config["data"]["window"] % 1.0, 0.0
-            ), "Frames mode not compatible with > 1 fractional windows"
-
-    if not args.debug:
-        # create directory for inference results
-        path_results = create_model_dir(args.path_results, args.runid)
-
-        # store validation settings
-        eval_id = log_config(path_results, args.runid, config)
-    else:
-        path_results = None
-        eval_id = -1
-
     # initialize settings
     device = config_parser.device
     kwargs = config_parser.loader_kwargs
-
-    # visualization tool
-    if config["vis"]["enabled"] or config["vis"]["store"]:
-        vis = Visualization(config, eval_id=eval_id, path_results=path_results)
 
     # model initialization and settings
     model = eval(config["model"]["name"])(config["model"]).to(device)
     model = load_model(args.runid, model, device)
     model.eval()
-
-    # validation metric
-    criteria = []
-    if "metrics" in config.keys():
-        for metric in config["metrics"]["name"]:
-            criteria.append(eval(metric)(config, device, flow_scaling=config["metrics"]["flow_scaling"]))
 
     # data loader
     data = H5Loader(config, config["model"]["num_bins"])
@@ -115,20 +67,22 @@ def test(args, config_parser):
     # print the summary of the model we are going to evaluate
     with torch.no_grad():
         printed_summary = False 
-        while True:
-            for inputs in dataloader:
+        for inputs in dataloader:
 
-                if not printed_summary:
-                    event_voxel = inputs["event_voxel"].to(device)
-                    event_cnt = inputs["event_cnt"].to(device)
+            if not printed_summary:
+                event_voxel = inputs["event_voxel"].to(device)
+                event_cnt = inputs["event_cnt"].to(device)
 
-                    summary(
-                        model,
-                        input_data=[event_voxel, event_cnt],
-                        device=device,
-                        col_names=["input_size", "output_size", "num_params"],
-                    )
-                    printed_summary = True
+                summary(
+                    model,
+                    input_data=[event_voxel, event_cnt],
+                    device=device,
+                    col_names=["input_size", "output_size", "num_params"],
+                )
+                printed_summary = True
+                    
+            break
+    mlflow.end_run()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
